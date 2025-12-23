802d52f963389e4e5db4a941451dafb1b7f6c9de
dev-osama74 <y648twm8sg@privaterelay.appleid.com>
2025-12-23 04:17:36 +0200

    Update - 2025-12-23 04:17

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // بدء الجلسة مع معالجة الأخطاء
 if (session_status() === PHP_SESSION_NONE) {
     @session_start();
